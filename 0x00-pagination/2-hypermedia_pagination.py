01fdb12522b87498f3475d48c06ce6c1ac8147a4
Robert Amoah <mr_robertamoah@yahoo.com>
2024-08-20 03:48:58 +0000

    done with 2-hypermedia_pagination

--- 0x00-pagination/2-hypermedia_pagination.py
+++ 0x00-pagination/2-hypermedia_pagination.py
@@ -52,3 +52,27 @@ class Server:
             return data[x:y]
         except IndexError:
             return []
+
+    def get_hyper(self, page: int = 1, page_size: int = 10) -> dict:
+        """
+        returns a dictionary containing pagination key-value pairs
+        """
+        data = self.get_page(page, page_size)
+        total_pages = math.ceil(len(self.dataset()) / page_size)
+
+        next_page = None
+        prev_page = None
+
+        if page < total_pages:
+            next_page = page + 1
+
+        if page > 1 and page <= total_pages:
+            prev_page = page - 1
+
+        return {'page_size': len(data),
+                'page': page,
+                'data': data,
+                'next_page': next_page,
+                'prev_page': prev_page,
+                'total_pages': total_pages
+                }
